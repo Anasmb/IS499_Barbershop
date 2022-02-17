ea4d55229ff449d2231ddbfdf885a41bf6566ced
Anasmb <40839455+Anasmb@users.noreply.github.com>
2022-02-17 19:35:31 +0300

    + Added sign up , sign in , Database Integration

--- app/src/main/java/com/example/barbershop/CreateAccountActivity.java
+++ app/src/main/java/com/example/barbershop/CreateAccountActivity.java
@@ -3,12 +3,32 @@ package com.example.barbershop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
     private ImageView backBtn;
+    private TextInputEditText name, email, password, phoneNumber;
+    private MaterialButton signupButton;
+    private TextView phoneNumberWarning, emailWarning, passwordWarning, nameWarning;
+
+    private boolean isNameValid = false;
+    private boolean isPhoneNumberValid = false;
+    private boolean isEmailValid = false;
+    private boolean isPasswordValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +37,175 @@ public class CreateAccountActivity extends AppCompatActivity {
 
         backBtn = findViewById(R.id.createAccount_back_button);
         backBtn.setOnClickListener(backBtnListener);
+
+        name = findViewById(R.id.createAccountNameEditText);
+        name.addTextChangedListener(nameInputCheck);
+        email = findViewById(R.id.createAccountEmailEditText);
+        email.addTextChangedListener(emailInputCheck);
+        password = findViewById(R.id.createAccountPasswordEditText);
+        password.addTextChangedListener(passwordInputCheck);
+        phoneNumber = findViewById(R.id.createAccountPhoneNumberEditText);
+        phoneNumber.addTextChangedListener(phoneNumberInputCheck);
+
+        signupButton = findViewById(R.id.createAccountSignupBtn);
+        signupButton.setOnClickListener(signupListener);
+
+        phoneNumberWarning = findViewById(R.id.mobileNumberWarning);
+        emailWarning = findViewById(R.id.emailWarning);
+        passwordWarning = findViewById(R.id.passwordWarning);
+        nameWarning = findViewById(R.id.nameWarning);
+
     }
 
     private View.OnClickListener backBtnListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             finish();
+        }
+    };
+
+    private View.OnClickListener signupListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            if(isNameValid && isEmailValid && isPhoneNumberValid && isPasswordValid) {
+            //Start ProgressBar first (Set visibility VISIBLE)
+            signupButton.setClickable(false);
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    //Starting Write and Read data with URL
+                    //Creating array for parameters
+                    String[] field = new String[4];
+                    field[0] = "name";
+                    field[1] = "email";
+                    field[2] = "password";
+                    field[3] = "phonenumber";
+                    //Creating array for data
+                    String[] data = new String[4];
+                    data[0] = String.valueOf(name.getText());
+                    data[1] = String.valueOf(email.getText());
+                    data[2] = String.valueOf(password.getText());
+                    data[3] = String.valueOf(phoneNumber.getText());
+                    Log.d("php" , data[0] + " " + data[1] + " " + data[2] + " " + data[3]);
+                    PutData putData = new PutData("http://192.168.100.6/barbershop-php/signup.php", "POST", field, data);
+                    if (putData.startPut()) {
+                        if (putData.onComplete()) {
+                            String result = putData.getResult();
+                            if(result.equals("Sign Up Success")){
+                                Toast.makeText(getApplicationContext(),"Register Success",Toast.LENGTH_LONG).show();
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+                                signupButton.setClickable(true);
+                            }
+                        }
+                    }
+                    //End Write and Read data with URL
+                }
+            });
+        }
+
+          }
+    };
+
+
+    private TextWatcher nameInputCheck = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if (editable.toString().length() < 1){
+                nameWarning.setVisibility(View.VISIBLE);
+                isNameValid = false;
+            }
+            else{
+                isNameValid = true;
+                nameWarning.setVisibility(View.INVISIBLE);
+            }
+        }
+    };
+
+    private TextWatcher emailInputCheck = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+            if(!Patterns.EMAIL_ADDRESS.matcher(editable.toString()).matches()){ // check the email format, is it valid or not?
+                emailWarning.setVisibility(View.VISIBLE);
+                isEmailValid = false;
+            }
+            else {
+                emailWarning.setVisibility(View.INVISIBLE);
+                isEmailValid = true;
+            }
+        }
+    };
+
+    private TextWatcher phoneNumberInputCheck = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if (editable.toString().length() != 10){
+                phoneNumberWarning.setVisibility(View.VISIBLE);
+                isPhoneNumberValid = false;
+            }
+            else {
+                phoneNumberWarning.setVisibility(View.INVISIBLE);
+                isPhoneNumberValid = true;
+            }
+        }
+    };
+
+    private TextWatcher passwordInputCheck = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if (editable.toString().length() < 8){
+                passwordWarning.setVisibility(View.VISIBLE);
+                isPasswordValid = false;
+            }
+            else {
+                passwordWarning.setVisibility(View.INVISIBLE);
+                isPasswordValid = true;
+            }
         }
     };
 
--- app/src/main/java/com/example/barbershop/SigninActivity.java
+++ app/src/main/java/com/example/barbershop/SigninActivity.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class SigninActivity extends AppCompatActivity {
 
+    private TextInputEditText email, password;
+    private MaterialButton signinButton;
     private ImageView backBtn;
     private TextView signupTxt;
 
@@ -18,11 +28,63 @@ public class SigninActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        email = findViewById(R.id.signinEmailEditText);
+        password = findViewById(R.id.signinPasswordEditText);
+
+        signinButton = findViewById(R.id.signinButton);
+        signinButton.setOnClickListener(signinListener);
+
         backBtn = findViewById(R.id.signin_back_button);
         backBtn.setOnClickListener(backBtnListener);
         signupTxt = findViewById(R.id.signupTxt);
         signupTxt.setOnClickListener(signupListener);
     }
+
+    private View.OnClickListener signinListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            if(email.getText().length() > 0 && password.getText().length() > 0) {
+                //Start ProgressBar first (Set visibility VISIBLE)
+                signinButton.setClickable(false);
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Starting Write and Read data with URL
+                        //Creating array for parameters
+                        String[] field = new String[2];
+                        field[0] = "email";
+                        field[1] = "password";
+                        //Creating array for data
+                        String[] data = new String[2];
+                        data[0] = String.valueOf(email.getText());
+                        data[1] = String.valueOf(password.getText());
+                        Log.d("php" , data[0] + " " + data[1]);
+                        PutData putData = new PutData("http://192.168.100.6/barbershop-php/login.php", "POST", field, data);
+                        if (putData.startPut()) {
+                            if (putData.onComplete()) {
+                                String result = putData.getResult();
+                                if(result.equals("Login Success")){
+                                    Log.d("php", result);
+                                    Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_LONG).show();
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
+                                    Log.d("php", result);
+                                    signinButton.setClickable(true);
+                                }
+                            }
+                        }
+                        //End Write and Read data with URL
+                    }
+                });
+            }
+
+
+        }
+    };
 
     private View.OnClickListener backBtnListener = new View.OnClickListener() {
         @Override
